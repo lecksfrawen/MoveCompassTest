5507017ef7223643ad737d85cdc382827dc9f747
Héctor De Diego <lecksfrawen@gmail.com>
2019-03-29 10:59:45 -0600

    Added POC

--- MoveCompassTest/ViewController.swift
+++ MoveCompassTest/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension MKMapView {
+    func getCompassView() -> UIView? {
+        return (self.subviews.filter { String(describing:$0).contains("MKCompassView") }.first)
     }
-
-
 }
 
+class SSMapView: MKMapView {
+    
+    private var _compassTopValue: CGFloat = 5
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let compassLeftValue = self.frame.width - 33 - 5
+        if let compassView = self.getCompassView() {
+            compassView.frame = CGRect(x:compassLeftValue, y:_compassTopValue, width:33, height:33)
+        }
+    }
+    
+    func setCompassPosition(top: CGFloat, animated: Bool) {
+        _compassTopValue = top
+        setNeedsLayout()
+        
+        if animated {
+            UIView.animate(withDuration: 0.3, delay: 0, options: [.curveEaseInOut], animations: {
+                self.layoutIfNeeded()
+            }, completion: nil)
+        } else {
+            self.layoutIfNeeded()
+        }
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapContainerView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var toggleArrow: UIButton!
+    
+    var mapView: SSMapView?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        iconImageView.image = UIImage(named: "dummy")
+        iconImageView.isHidden = true
+        
+        toggleArrow.setTitle("Show arrow", for: .normal)
+        toggleArrow.setTitle("Hide arrow", for: .selected)
+        
+        if mapView?.superview == nil {
+            mapView = SSMapView( frame: .zero )
+            if let mapView = mapView {
+                configureMap(map: mapView)
+            }
+        }
+    }
+    
+    func configureMap(map: SSMapView) {
+        map.delegate = self
+        map.translatesAutoresizingMaskIntoConstraints = false
+        mapContainerView.addSubview(map)
+        map.leftAnchor.constraint(equalTo: mapContainerView.leftAnchor).isActive = true
+        map.topAnchor.constraint(equalTo: mapContainerView.topAnchor).isActive = true
+        map.rightAnchor.constraint(equalTo: mapContainerView.rightAnchor).isActive = true
+        map.bottomAnchor.constraint(equalTo: mapContainerView.bottomAnchor).isActive = true
+        map.userTrackingMode = .none
+        map.isRotateEnabled = true
+        map.showsUserLocation = true
+    }
+    
+    @IBAction func toogleArrowAction(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        iconImageView.isHidden = !sender.isSelected
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        print("regionWillChangeAnimated")
+    }
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        print("mapViewDidChangeVisibleRegion")
+        guard let mapView = mapView as? SSMapView else { return }
+        let compassHeight: CGFloat = iconImageView.isHidden ? 5 : 80
+        mapView.setCompassPosition(top: compassHeight, animated: false)
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("regionDidChangeAnimated")
+    }
+}

1a61e0476b0f1b8ab49bd2e97d1ca126af0dcb27
Héctor De Diego <lecksfrawen@gmail.com>
2019-03-29 09:26:53 -0600

    Initial Commit

---
+++ MoveCompassTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveCompassTest
+//
+//  Created by Hector de Diego on 19/03/29.
+//  Copyright © 2019 hector.dd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
